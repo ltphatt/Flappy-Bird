c23540c38a122f9b9500ade184c59871a92742a0
Lê Thanh Phát <ltphatt2709@gmail.com>
2023-09-06 21:19:10 +0700

    added

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -8,22 +8,10 @@ public class GameOver : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI resultScoreText;
     [SerializeField] TextMeshProUGUI resultHighScoreText;
-    [SerializeField] Image medal;
-    [SerializeField] List<Image> medals;
 
     void Start()
     {
         resultScoreText.text = Score.instance.GetCurrentScore().ToString();
         resultHighScoreText.text = Score.instance.GetHighScore().ToString();
-    }
-
-    void Update()
-    {
-
-    }
-
-    void ChangeMedal()
-    {
-
     }
 }
